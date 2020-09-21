f6a48837b9d4217b6f8fc4532e0c90bfb3a21c8b
Vinicius Viegas <vuviegas85@gmail.com>
2020-09-21 15:38:11 -0300

    Images Upload Completed

---
+++ db/migrate/20200921153532_remove_image_ur_lfrom_cocktails.rb
@@ -0,0 +1,5 @@
+class RemoveImageUrLfromCocktails < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :cocktails, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_151953) do
+ActiveRecord::Schema.define(version: 2020_09_21_153532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_151953) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image_url"
   end
 
   create_table "doses", force: :cascade do |t|
